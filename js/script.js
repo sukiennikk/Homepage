b695c5bcdf3cd3ca91fc529740b5ef1f806586e3
Katarzyna Sukiennik <katarzyna.sukiennik99@gmail.com>
2025-03-16 20:21:16 +0100

    function onChangeBackgroundClick added

--- js/script.js
+++ js/script.js
@@ -1,12 +1,14 @@
+let onChangeBackgroundClick = () => {
+    document.body.classList.toggle("dark-mode");
+ 
+ 
+    if (document.body.classList.contains("dark-mode")) {
+        button.textContent = "Wyłącz ciemny motyw";
+    } else {
+        button.textContent = "Włącz ciemny motyw";
+    }
+ };
+
 let button = document.querySelector(".button");
 
-button.addEventListener("click", () => {
-   document.body.classList.toggle("dark-mode");
-
-
-   if (document.body.classList.contains("dark-mode")) {
-       button.textContent = "Wyłącz ciemny motyw";
-   } else {
-       button.textContent = "Włącz ciemny motyw";
-   }
-});
+button.addEventListener("click", onChangeBackgroundClick);
